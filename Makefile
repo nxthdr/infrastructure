2c32946f50d532fc986ca8cb7bd24691ecb4d10a
matthieugouel <matthieu.gouel@protonmail.com>
2025-01-10 19:26:33 +0100

    remove caddy on waw scaleway instance

--- Makefile
+++ Makefile
@@ -1,13 +1,11 @@
 .PHONY: sync-cert
 sync-cert:
 	scp -r root@core.infra.nxthdr.dev:/home/nxthdr/proxy/data/caddy/certificates/ root@ams.scw.infra.nxthdr.dev:/home/nxthdr/proxy/data/caddy/
-	scp -r root@core.infra.nxthdr.dev:/home/nxthdr/proxy/data/caddy/certificates/ root@waw.scw.infra.nxthdr.dev:/home/nxthdr/proxy/data/caddy/
 
 .PHONY: sync-config
 sync-config:
 	rsync -r files/core/* nxthdr@core.infra.nxthdr.dev:~
 	rsync -r files/ams-sw/* nxthdr@ams.scw.infra.nxthdr.dev:~
-	rsync -r files/waw-sw/* nxthdr@waw.scw.infra.nxthdr.dev:~
 
 .PHONY: apply
 apply: sync-config
--- docker-waw-sw.tf
+++ docker-waw-sw.tf
@@ -1,26 +1,26 @@
-resource "docker_image" "waw_sw_caddy" {
-  name = "caddy:2.9"
-  provider = docker.waw_sw
-}
+# resource "docker_image" "waw_sw_caddy" {
+#   name = "caddy:2.9"
+#   provider = docker.waw_sw
+# }
 
-resource "docker_container" "waw_sw_proxy" {
-  image = docker_image.waw_sw_caddy.image_id
-  name  = "proxy"
-  provider = docker.waw_sw
-  log_driver = "json-file"
-  log_opts = {
-    tag = "{{.ImageName}}|{{.Name}}|{{.ImageFullID}}|{{.FullID}}"
-  }
-  dns = [ "2a00:1098:2c::1", "2a00:1098:2c::1", "2a00:1098:2b::1" ]
-  env = ["CADDY_ADMIN=[::]:2019"]
-#   user = "1000:1000"
-  network_mode = "host"
-  volumes {
-    container_path = "/etc/caddy/Caddyfile"
-    host_path = "/home/nxthdr/proxy/config/Caddyfile"
-  }
-  volumes {
-    container_path = "/data"
-    host_path = "/home/nxthdr/proxy/data"
-  }
-}
+# resource "docker_container" "waw_sw_proxy" {
+#   image = docker_image.waw_sw_caddy.image_id
+#   name  = "proxy"
+#   provider = docker.waw_sw
+#   log_driver = "json-file"
+#   log_opts = {
+#     tag = "{{.ImageName}}|{{.Name}}|{{.ImageFullID}}|{{.FullID}}"
+#   }
+#   dns = [ "2a00:1098:2c::1", "2a00:1098:2c::1", "2a00:1098:2b::1" ]
+#   env = ["CADDY_ADMIN=[::]:2019"]
+# #   user = "1000:1000"
+#   network_mode = "host"
+#   volumes {
+#     container_path = "/etc/caddy/Caddyfile"
+#     host_path = "/home/nxthdr/proxy/config/Caddyfile"
+#   }
+#   volumes {
+#     container_path = "/data"
+#     host_path = "/home/nxthdr/proxy/data"
+#   }
+# }
