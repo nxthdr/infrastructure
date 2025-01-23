8064db6f92a2f14bf6950d1c8b3bc830a290a120
matthieugouel <matthieu.gouel@protonmail.com>
2025-01-23 22:42:42 +0100

    update saimiris table schema

--- clickhouse-tables/saimiris/saimiris.sql
+++ clickhouse-tables/saimiris/saimiris.sql
@@ -1,7 +1,7 @@
 CREATE TABLE saimiris.from_kafka
 (
     timestamp DateTime64,
-    prober_id UInt16,
+    prober_id String,
     reply_src_addr IPv6,
     reply_dst_addr IPv6,
     reply_id UInt16,
@@ -32,7 +32,7 @@ SETTINGS
 CREATE TABLE saimiris.results
 (
     timestamp DateTime64,
-    prober_id UInt16,
+    prober_id String,
     reply_src_addr IPv6,
     reply_dst_addr IPv6,
     reply_id UInt16,
