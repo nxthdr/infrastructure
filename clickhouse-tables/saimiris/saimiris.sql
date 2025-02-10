a73c2b5a0839666c2fa8a163bcf3613e7fdef654
matthieugouel <matthieu.gouel@protonmail.com>
2025-02-10 23:02:22 +0100

    add `kafka_max_rows_per_message` to `saimris.from_kafka` table

--- clickhouse-tables/saimiris/saimiris.sql
+++ clickhouse-tables/saimiris/saimiris.sql
@@ -27,7 +27,8 @@ SETTINGS
     kafka_broker_list = '[2a06:de00:50:cafe:10::103]:9092',
     kafka_topic_list = 'saimiris-results',
     kafka_group_name = 'clickhouse-saimiris-group',
-    kafka_format = 'CSV';
+    kafka_format = 'CSV',
+    kafka_max_rows_per_message = 1048576;
 
 CREATE TABLE saimiris.results
 (
