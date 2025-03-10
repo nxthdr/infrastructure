46787697a60414385d030c8f82d84f1742f7cf8c
matthieugouel <matthieu.gouel@protonmail.com>
2025-03-10 15:53:48 +0100

    improve saimprowler script

--- templates/scw_core/cronjobs/saimprowler/saimprowler.sh
+++ templates/scw_core/cronjobs/saimprowler/saimprowler.sh
@@ -2,14 +2,17 @@
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+DOCKER_IMAGE_PROWL=ghcr.io/nxthdr/prowl:main
+DOCKER_IMAGE_SAIMIRIS=ghcr.io/nxthdr/saimiris:main
+
 # Always Docker pull the latest image
-docker pull ghcr.io/nxthdr/prowl:main
-docker pull ghcr.io/nxthdr/saimiris:main
+docker pull $DOCKER_IMAGE_PROWL
+docker pull $DOCKER_IMAGE_SAIMIRIS
 
 # Probes generation
 docker run --rm \
     -v $SCRIPTPATH/data/targets.csv:/data/targets.csv \
-    ghcr.io/nxthdr/prowl:main \
+    $DOCKER_IMAGE_PROWL \
     --tool traceroute --mapper sequential \
     /data/targets.csv > $SCRIPTPATH/data/probes.csv
 
@@ -17,5 +20,5 @@ docker run --rm \
 docker run --rm --network=host \
     -v $SCRIPTPATH/config/config.yml:/config/config.yml \
     -v $SCRIPTPATH/data/probes.csv:/data/probes.csv \
-    ghcr.io/nxthdr/saimiris:main \
+    $DOCKER_IMAGE_SAIMIRIS \
     client --config /config/config.yml --probes-file=/data/probes.csv d2nm65zx8n,j7fph85rgr
