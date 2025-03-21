d630fe21117c1b98676a778358833aeaf24809f2
matthieugouel <matthieu.gouel@protonmail.com>
2025-03-21 18:33:28 +0100

    add rsyslog loki ingestion from scwams01

--- terraform/docker-scw-ams01.tf
+++ terraform/docker-scw-ams01.tf
@@ -1,3 +1,10 @@
+resource "docker_network" "scw_ams01_backend" {
+  name = "backend"
+  provider = docker.scw_ams01
+  driver = "bridge"
+  ipv6 = true
+}
+
 resource "docker_image" "scw_ams01_caddy" {
   name = "caddy:2.9"
   provider = docker.scw_ams01
@@ -22,5 +29,47 @@ resource "docker_container" "scw_ams01_proxy" {
   volumes {
     container_path = "/data"
     host_path = "/home/nxthdr/proxy/data"
+  }
+}
+
+resource "docker_image" "scw_ams01_alloy" {
+  name = "grafana/alloy:v1.7.5"
+  provider = docker.scw_ams01
+}
+
+resource "docker_container" "scw_ams01_alloy" {
+  image = docker_image.scw_ams01_alloy.image_id
+  name  = "alloy"
+  provider = docker.scw_ams01
+  command = [
+    "run", "--storage.path=/var/lib/alloy/data",
+    "/etc/alloy/config.alloy"
+  ]
+  log_driver = "json-file"
+  log_opts = {
+    tag = "{{.ImageName}}|{{.Name}}|{{.ImageFullID}}|{{.FullID}}"
+  }
+  dns = [ "2a00:1098:2c::1", "2a00:1098:2c::1", "2a00:1098:2b::1" ]
+  network_mode = "bridge"
+  networks_advanced {
+    name = docker_network.ixp_fra01_backend.name
+  }
+  ports {
+    internal = 514
+    external = 514
+    protocol = "udp"
+  }
+  ports {
+    internal = 601
+    external = 601
+    protocol = "tcp"
+  }
+  volumes {
+    container_path = "/etc/alloy/config.alloy"
+    host_path = "/home/nxthdr/alloy/config/config.alloy"
+  }
+  volumes {
+    container_path = "/var/lib/alloy/data"
+    host_path = "/home/nxthdr/alloy/data"
   }
 }
